78a8de4ee536882c73808757dac490f0eac97968
anhkhoa1101 <email-cua-ban@example.com>
2026-06-17 20:50:33 +0700

    Update member1_v2

--- src/main/java/com/mycompany/restaurantmanagement/model/Employee.java
+++ src/main/java/com/mycompany/restaurantmanagement/model/Employee.java
@@ -1,5 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.restaurantmanagement.model;
 
+/**
+ *
+ * @author khoa0
+ */
 import java.util.ArrayList;
 import java.util.List;
 
