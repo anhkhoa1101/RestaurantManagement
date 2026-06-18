27753fadf345aad1ed73ef64a3560819b6bcc041
anhkhoa1101 <email-cua-ban@example.com>
2026-06-18 16:53:06 +0700

    Bổ sung file vào service

---
+++ src/main/java/com/mycompany/restaurantmanagement/repository/MenuItemRepository.java
@@ -0,0 +1,4 @@
+package com.mycompany.restaurantmanagement.repository;
+
+public class MenuItemRepository {
+}
---
+++ src/main/java/com/mycompany/restaurantmanagement/service/TableService.java
@@ -0,0 +1,4 @@
+package com.mycompany.restaurantmanagement.service;
+
+public class TableService {
+}
