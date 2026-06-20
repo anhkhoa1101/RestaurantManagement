70bdfaffb6a72760d1f29a00f6f5d7b991614b32
tranlenghia <tranlenghiac0821@gmail.com>
2026-06-20 18:56:53 +0700

    Update OrderDetailService.java
    orderdetailservice

--- src/main/java/com/mycompany/restaurantmanagement/service/OrderDetailService.java
+++ src/main/java/com/mycompany/restaurantmanagement/service/OrderDetailService.java
@@ -1,4 +1,40 @@
 package com.mycompany.restaurantmanagement.service;
 
+
+
+import com.mycompany.restaurantmanagement.model.Order;
+import com.mycompany.restaurantmanagement.model.MenuItem;
+
 public class OrderDetailService {
+    private OrderService orderService; 
+
+    public OrderDetailService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    
+    public void addItemToTable(int tableId, MenuItem item, int quantity) {
+        Order activeOrder = orderService.getActiveOrderByTable(tableId);
+        
+        if (activeOrder == null) {
+            System.out.println("❌ Error: Cannot add items. This table has no active order.");
+            return;
+        }
+        
+        
+        activeOrder.addDetail(item, quantity);
+    }
+
+    
+    public void removeItemFromTable(int tableId, MenuItem item) {
+        Order activeOrder = orderService.getActiveOrderByTable(tableId);
+        
+        if (activeOrder == null) {
+            System.out.println("❌ Error: Cannot remove items. This table has no active order.");
+            return;
+        }
+        
+       
+        activeOrder.removeDetail(item);
+    }
 }
