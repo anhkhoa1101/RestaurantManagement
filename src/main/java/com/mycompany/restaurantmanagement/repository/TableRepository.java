bd3ab528f4f25f53d84941523858df2cd62cf8b8
tranlenghia <tranlenghiac0821@gmail.com>
2026-06-21 16:33:40 +0700

    Update TableRepository.java
    tableRepository

--- src/main/java/com/mycompany/restaurantmanagement/repository/TableRepository.java
+++ src/main/java/com/mycompany/restaurantmanagement/repository/TableRepository.java
@@ -1,4 +1,24 @@
 package com.mycompany.restaurantmanagement.repository;
-
+import com.mycompany.restaurantmanagement.model.Table;
+import java.util.ArrayList;
+import java.util.List;
 public class TableRepository {
+    private final List<Table> tableList = new ArrayList<>();
+    public List<Table> findAll() {
+        return this.tableList;
+    }
+    public void save(Table table) {
+        if (table != null) {
+            this.tableList.add(table);
+        }
+    }
+    public Table findById(int tableId) {
+        for (int i=0; i < tableList.size(); i++) {
+            Table currentTable = tableList.get(i);
+            if (currentTable.getTableId() == tableId) {
+                return currentTable;
+            }
+        }
+        return null;
+    }
 }
