aa37843137a399a5a778e0be17127ad585675c1d
tranlenghia <tranlenghiac0821@gmail.com>
2026-06-21 16:53:18 +0700

    Update OrderRepository.java
    orderrepository

--- src/main/java/com/mycompany/restaurantmanagement/repository/OrderRepository.java
+++ src/main/java/com/mycompany/restaurantmanagement/repository/OrderRepository.java
@@ -1,5 +1,25 @@
 package com.mycompany.restaurantmanagement.repository;
-
+import com.mycompany.restaurantmanagement.model.Order;
+import java.util.ArrayList;
+import java.util.List;
 public class OrderRepository {
-    
+    private final List<Order> orderList = new ArrayList<>();
+    public List<Order> findAll() {
+        return this.orderList;
+    }
+    public void save(Order order) {
+        if (order !=null) {
+            this.orderList.add(order);
+        }
+    }
+    public Order findById(String orderId) {
+        for (int i=0; i < orderList.size(); i++) {
+            Order currentOrder = orderList.get(i);
+            if (currentOrder.getOrderId().equals(orderId)) {
+                return currentOrder;
+            }
+        }
+        return null;
+    }
 }
+
